e6bdbdbb6b3308f782aa5dee5ef9627cff8d4ce3
dkeum <dkeum@sfu.ca>
2025-09-22 23:53:53 -0700

    testing 1

--- server/server.js
+++ server/server.js
@@ -13,18 +13,18 @@ await connectCloudinary();
 const app = express();
 
 
-app.use(cors());
-app.use(express.json());
-app.use(clerkMiddleware());
+// app.use(cors());
+// app.use(express.json());
+// app.use(clerkMiddleware());
 
-app.use("/api/ai", router);
-app.use('/api/user', userRouter)
+// app.use("/api/ai", router);
+// app.use('/api/user', userRouter)
 
-app.get("/", (req, res) => {
-  res.send("Server is Live!");
-});
+// app.get("/", (req, res) => {
+//   res.send("Server is Live!");
+// });
 
-app.use(requireAuth());
+// app.use(requireAuth());
 
 const PORT = process.env.PORT || 3000;
 
